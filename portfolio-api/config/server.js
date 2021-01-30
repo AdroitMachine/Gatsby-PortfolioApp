7452edfa7e50105e79dd387fab3d3494b739c3c0
AdroitMachine <anujsaxenamail@gmail.com>
2021-01-30 20:40:47 +0530

    initial commit

---
+++ portfolio-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '17f28acf70040056a63c8b611b902c4e'),
+    },
+  },
+});
---
+++ portfolio-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fdc31a50-bde4-4a42-83a0-b92138b87abd'
+};
